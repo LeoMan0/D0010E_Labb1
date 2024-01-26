bf72363fe3efb441855373e3d68bbc41b6611d0a
LeoMan0 <leoman3628@gmail.com>
2024-01-26 04:41:56 +0100

    Clean up of part 1 left. And part 2

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
             //Doing the stuff if the input is accepted
             if (userInt > 0){
+                // Here is we print the stuff for task 1 and 2
                 System.out.println("Number in is:  " + userInt);
                 System.out.println("Number out  for f1 is: " + f1(userInt));
                 System.out.println("Number our for f2 is: " + f2(userInt));
@@ -24,6 +25,14 @@ public class Main {
                 System.out.println("Number out for f8 is: " + f8(userInt));
                 System.out.println("Number out for f16 is: " + f16(userInt));
                 System.out.println("Number out for f32 is: " + f32(userInt));
+
+                //Printing task 3 unclear what more to do
+                System.out.println("Number out for iterateF(a4,userInt) is:" + iterateF(1, userInt));
+
+                //Task 4 sout.
+                System.out.println(intsToString(userInt, iterLifeLength(userInt)));
+
+
             }
             else if (userInt == 0) {
                 System.out.println("Loop is closed zero was given");
@@ -34,7 +43,12 @@ public class Main {
                 System.out.println("Is less then zero error");
             }
 
+        }
 
+        //Task 4 part 3 AKA numbers from 1 to 15 prints
+        for (int i=1; i <= 15; i++ ){
+            System.out.println(intsToString(i,iterLifeLength(i)));
+            System.out.println(intsToString(i,recLifeLength(i)));
         }
 
     }
@@ -83,10 +97,41 @@ public class Main {
         return f16(f16(a0));
     }
 
+    // Task 3 method
     public static int iterateF(int a0, int n){
+        int iterateFOut = n;
 
-        return 0;
+        for (int i = 0; i < a0; i++){
+            iterateFOut=f1(iterateFOut);
 
+        }
+
+        return iterateFOut;
+    }
+
+    //Task 4 method
+    public static int iterLifeLength(int a0){
+        int lifeLength=0;
+
+        while (a0 != 1){
+            a0 = f1(a0);
+            lifeLength++;
+        }
+        return lifeLength;
+    }
+
+    public static String intsToString(int numberIn, int lifeLengthOfNumberIn){
+        return ("The life length of " + numberIn + " is " + lifeLengthOfNumberIn + ".");
+
+    }
+
+    //Task 5 method
+    public static int recLifeLength(int a0) {
+        if (a0 == 1) {
+            return 0; // Base case: life length of 1 is 0
+        } else {
+            return 1 + recLifeLength(f1(a0));
+        }
     }
 
 }
